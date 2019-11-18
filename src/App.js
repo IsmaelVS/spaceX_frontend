de162859e7c4b1e628f19ba8bb4ea9f2b64abcaf
Ismael Ventura <ismael.venturadasilva0@gmail.com>
2019-11-18 13:28:38 -0300

    Alterando filter para função, adicionando validação para busca não encontrada e removendo arquivos desnecessários.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import api from "./services/api";
 export default function PastCapsules() {
   const [capsules, setCapsules] = useState([]);
   const [search, setSearch] = useState("");
-  const [value, setValue] = useState("all");
+  const [value, setValue] = useState("all_capsules");
 
   useEffect(() => {
     api
-      .get("")
+      .get("all_capsules")
       .then(response => {
         setCapsules(response.data);
       })
@@ -26,11 +26,7 @@ export default function PastCapsules() {
     api
       .get(path)
       .then(response => {
-        // ({
-        //   'past': setPast,
-        //   'upcoming': setupcoming
-        // })[path](response.data)
-        setCapsules(response.data);
+        setCapsules(Array.isArray(response.data) ? response.data : [response.data]);
       })
       .catch(error => {
         console.log(error);
@@ -42,44 +38,50 @@ export default function PastCapsules() {
     handleApi(e.target.value);
   }
 
+  const filteredCapsules = capsules
+  .filter(capsule =>
+    search
+      ? capsule.capsule_serial
+          .toLowerCase()
+          .includes(search.toLowerCase())
+      : true
+  )
+
   return (
     <div className="App-container">
       <header className="App-header">
-        <form>
-          <input type="text" value={search} onChange={handleSearchInput} />
-        </form>
-        <select value={value} onChange={handleChange}>
-        <option value="all">Todos os lançamentos</option>
-        <option value="past">Lançamentos passados</option>
-        <option value="upcoming">Último lançamento</option>
-        <option value="upcomings">Próximos lançamentos</option>
-        <option value="upcomingss">Próximo lançamento</option>
-        </select>
-        </header>
+        <span>Space X</span>
+        <div className="App-header-filter">
+          <input type="text" value={search} onChange={handleSearchInput} placeholder="Pesquisar serial do lançamento" />
+          <select value={value} onChange={handleChange}>
+          <option value="all_capsules">Todos os lançamentos</option>
+          <option value="past_capsules">Lançamentos passados</option>
+          <option value="past_capsule">Último lançamento</option>
+          <option value="upcoming_capsules">Próximos lançamentos</option>
+          <option value="upcoming_capsule">Próximo lançamento</option>
+          </select>
+        </div>
+      </header>
+      <div className="App-list">
+        {filteredCapsules.length ?
         <ul className="App-capsule-list">
-          {capsules
-            .filter(capsule =>
-              search
-                ? capsule.capsule_serial
-                    .toLowerCase()
-                    .includes(search.toLowerCase())
-                : true
-            )
-            .map(capsule => (
-              <li key={capsule.capsule_serial}>
-                <p>Id: {capsule.capsule_id}</p>
-                <p>Serial: {capsule.capsule_serial}</p>
-                <p>Details: {capsule.details}</p>
-                <p>Landings: {capsule.landings}</p>
-                <p>Original Launch: {capsule.original_launch}</p>
-                <p>Original Launch Unix: {capsule.original_launch_unix}</p>
-                <p>reuse Count: {capsule.reuse_count}</p>
-                <p>Status: {capsule.status}</p>
-                <p>Type: {capsule.type}</p>
-              </li>
-            ))}
+          {filteredCapsules.map(capsule => (
+            <li key={capsule.capsule_serial}>
+              <p>Id: {capsule.capsule_id}</p>
+              <p>Serial: {capsule.capsule_serial}</p>
+              <p>Details: {capsule.details}</p>
+              <p>Landings: {capsule.landings}</p>
+              <p>Original Launch: {capsule.original_launch}</p>
+              <p>Original Launch Unix: {capsule.original_launch_unix}</p>
+              <p>reuse Count: {capsule.reuse_count}</p>
+              <p>Status: {capsule.status}</p>
+              <p>Type: {capsule.type}</p>
+            </li>
+          ))}
         </ul>
-        // <p>{!capsules.length && "Nenhum lançamento encontrado"}</p>
+        : <span> Nenhum lançamento encontrado </span>
+        }
+      </div>
     </div>
   );
 }
